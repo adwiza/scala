694e5bf2c9135d3686a066f64dfb748354aa5680
Aleksei Elokhin <adwiza@gmail.com>
2022-11-10 08:21:15 +0300

    Prod model finished

--- src/main/scala/crispdmProd.scala
+++ src/main/scala/crispdmProd.scala
@@ -1,24 +1,50 @@
-import org.apache.spark.ml.feature.Intercept
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
+package learn.sparkml
 
-object crispdmProd extends App {
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.ml.PipelineModel
 
-  val spark = SparkSession
-    .builder()
-    .appName("CRISP-DM")
-    .master("local")
-    .getOrCreate()
+object crispdmProd {
+  def main (args: Array[String]): Unit = {
+    if (args.length != 3) {
+      println("Usage: crispdmProd <path-to-model> <path-to-input> <path-to-output>")
+      sys.exit(-1)
+    }
 
-  import spark.implicits._
+    val spark = SparkSession
+      .builder()
+      .appName("crispdmProd")
+      .config("spark.sql.debug.maxToStringFields", 100)
+      .getOrCreate()
 
+    import spark.implicits._
 
-  //  Load data
-  val raw = spark
-    .read
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .csv("/Users/adwiz/IdeaProjects/crispdm/BankChurners-1801-1d994f.csv")
+    try {
+      val model = PipelineModel.load(args(0))
+      //  Load data
+      val data = spark
+        .read
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .csv(args(1))
 
+      //  Load model
+      import org.apache.spark.ml.PipelineModel
+
+      // Calculating
+      val prediction = model.transform(data)
+
+      prediction
+        .filter($"prediction" === 1)
+        .select("CLIENTNUM")
+        .repartition(1)
+        .write
+        .mode(SaveMode.Overwrite)
+        .csv(args(2))
+
+    } catch {
+      case e: Exception =>
+        println(s"ERROR: ${e.getMessage}")
+        sys.exit(-1)
+    }
+  }
 }
